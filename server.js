5594a2a2d4601a8b3cbf27c27e48e3ddfc2a8456
leonardommascarenhas <leonardommascarenhas@gmail.com>
2022-12-21 18:58:58 -0300

    second commit

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
+
+  //lodash
+
+  const num = _.random(0, 20);
+  console.log(num);
 
   let path = "./pages/";
   switch (req.url) {
     case "/":
       path += "index.html";
+      res.statusCode = 200;
       break;
     case "/about":
       path += "about.html";
+      res.statusCode = 200;
       break;
     default:
       path += "404.html";
+      res.statusCode = 404;
       break;
   }
 
